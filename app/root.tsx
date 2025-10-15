7da6604b0d519a28522d9d5fc1d10c0a181d556b
Miaseodov Tony <myasoedov.blog@gmail.com>
2025-10-15 21:15:48 +0300

    Refactor

--- app/root.tsx
+++ app/root.tsx
@@ -7,8 +7,11 @@ import {
   ScrollRestoration,
 } from "react-router";
 
-import type { Route } from "./+types/root";
+import '@/application/styles/tailwind.css'
 import "./app.css";
+
+import type { Route } from "./+types/root";
+
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -21,18 +24,29 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel: "stylesheet", 
+    href: "https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap",
+  },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html 
+      lang="en"
+      className="h-full scroll-smooth bg-white antialiased"
+      style={{
+        '--font-inter': 'Inter, sans-serif',
+        '--font-lexend': 'Lexend, sans-serif'
+      } as React.CSSProperties}
+    >
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="flex h-full flex-col">
         {children}
         <ScrollRestoration />
         <Scripts />
